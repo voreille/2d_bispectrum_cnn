b81582a2169b09d1b2d33887380e425626949e38
Val <valentin.oreiller@gmail.com>
2021-12-16 10:29:58 +0100

    update from ultrafast

--- src/models/monuseg/evaluation.py
+++ src/models/monuseg/evaluation.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from itertools import combinations
 
 import numpy as np
+from tqdm import tqdm
 import pandas as pd
 import tensorflow as tf
 from skimage.measure import label
@@ -41,7 +43,7 @@ def evaluate_equivariance(
     if id_list is not None:
         image_ids = [i for i in image_ids if i in id_list]
     results = pd.DataFrame()
-    for image_id in image_ids:
+    for image_id in tqdm(image_ids):
         path_image = tf.strings.join([image_dir, "/", image_id, ".tiff"])
         image = load_image_tif(path_image, normalizing_factor=255.0).numpy()
         images = [image]
@@ -49,23 +51,26 @@ def evaluate_equivariance(
             images.append(rotate(image, rot, reshape=False))
 
         images = np.stack(images, axis=0)
-        preds = model.predict(images, batch_size=1)
+        preds = model.predict(images, batch_size=4)
         pp_preds = post_processing(preds) != 0
 
         results_dict = {}
         for i, rot in enumerate([-90, -180, -270]):
-            pred_rotated = rotate(preds[i + 1, ...], rot)
-            pp_pred_rotated = rotate(pp_preds[i + 1, ...], rot)
+            preds[i + 1, ...] = rotate(preds[i + 1, ...], rot)
+            pp_preds[i + 1, ...] = rotate(pp_preds[i + 1, ...], rot)
 
+        rotations = [0, 90, 180, 270]
+        for i, k in combinations([0, 1, 2, 3], 2):
             results_dict.update({
-                f"RMSE_{-rot}deg_core":
-                rmse(preds[0, :, :, 0], pred_rotated[..., 0]),
-                f"RMSE_{-rot}deg_border":
-                rmse(preds[0, :, :, 1], pred_rotated[..., 1]),
-                f"RMSE_{-rot}deg_background":
-                rmse(preds[0, :, :, 2], pred_rotated[..., 2]),
-                f"dice_{-rot}deg":
-                dice(pp_preds[0, ...], pp_pred_rotated),
+                "rot_pair": (rotations[i], rotations[k]),
+                "RMSE_core":
+                rmse(preds[i, :, :, 0], preds[k, :, :, 0]),
+                "RMSE_border":
+                rmse(preds[i, :, :, 1], preds[k, :, :, 1]),
+                "RMSE_background":
+                rmse(preds[i, :, :, 2], preds[k, :, :, 2]),
+                "dice":
+                dice(pp_preds[i, ...], pp_preds[k, ...]),
             })
 
         results = results.append(
